062ede873337c9022f0779322d18e957cf30ef3a
GalaxyPlayzXD2 <GalaxyPlayzXDofficial@gmail.com>
2026-06-10 09:07:53 +0530

    Test action

--- Bot/src/index.js
+++ Bot/src/index.js
@@ -1,1 +1,31 @@
-console.log("GalaxyAI Bot v0.1.0");
+const mineflayer = require('mineflayer')
+const config = require('../config.json')
+const TestActionJump = require('./actions/TestActionJump')
+
+const bot = mineflayer.createBot({
+  host: config.host,
+  port: config.port,
+  username: config.username,
+  version: config.version
+})
+
+bot.on('spawn', () => {
+  console.log('✅ Bot joined the server!')
+})
+
+bot.on('end', () => {
+  console.log('❌ Bot disconnected')
+})
+
+bot.on('error', (err) => {
+  console.log('⚠️ Error:', err.message)
+})
+bot.on('chat', (username, message) => {
+
+    if (username === bot.username) return
+
+    if (message === '!jump') {
+        TestActionJump.execute(bot)
+    }
+
+})
